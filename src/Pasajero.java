dceae46e10763827dcf2dd308dbd52d60911a11a
maximonavarrete2301-sys <maximo.navarrete2301@alumnos.ubiobio.cl>
2026-04-23 20:43:56 -0400

    arreglo clase persona y cliente

--- src/Pasajero.java
+++ src/Pasajero.java
@@ -1,22 +1,4 @@
-public class Pasajero extends Persona {
-    private nombre =nomContacto;
-    private String fonoContacto;
-}
- public Pasajero(IdPersona id, Nombre nombre , Nombre nomContacto , String fonoContacto ) {
-    super(id,nombre)
-    this.nomContacto = nomContacto ;
-    this.fonoContacto = fonoContacto;
-}
-public Nombre getNomContacto() {
-    return nomContacto;
-}
-public void setNomContacto(Nombre nom) {
-    this.nomContacto = nom;
-}
-public String getFonoContacto() {
- return fonoContacto;
-}
-public void setFonoContacto(String fono) {
-    this.fonoContacto = fono;
-}
+package PACKAGE_NAME;
+
+public class Pasajero {
 }
