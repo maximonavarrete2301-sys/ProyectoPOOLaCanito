7b1223aba8f758bfcabc21f90c8f2d62a1f4580a
victor <victor.diaz2501@alumnos.ubiobio.cl>
2026-05-25 13:59:13 -0400

    Bus, Pago, PagoEfectivo, PagoTarjeta add

---
+++ src/modelo/PagoTarjeta.java
@@ -0,0 +1,22 @@
+package modelo;
+
+//Victor Diaz
+
+public class PagoTarjeta extends Pago {
+
+    private long nroTarjeta;
+
+    public PagoTarjeta(int monto, long nroTarjeta){
+        super(monto);
+        this.nroTarjeta=nroTarjeta;
+
+    }
+
+    public  long getNroTarjeta(){
+
+        return nroTarjeta;
+    }
+
+
+
+}
