5188ec3269d2a406ebaf4636eaa0867a3c679ebc
victor <victor.diaz2501@alumnos.ubiobio.cl>
2026-05-25 01:20:00 -0400

    avance 2

--- src/modelo/Bus.java
+++ src/modelo/Bus.java
@@ -1,50 +1,60 @@
-//MAXIMO NAVARRETE FERNANDEZ Victor Diaz
+package modelo;
+
 import java.util.ArrayList;
+
 public class Bus {
     private String patente;
     private String marca;
     private String modelo;
     private int nroAsientos;
-    private ArrayList<Viaje> viajes;
     private Empresa empresa;
+    private ArrayList<Viaje> viajes;
 
-    public Bus(String patente, int nroAsientos, Empresa emp) {
-        this.patente=patente;
-        this.nroAsientos=nroAsientos;
-        this.viajes= new ArrayList<>();
-        this.empresa=emp;
-
+    public Bus(String patente, int nroAsientos, Empresa empresa) {
+        this.patente = patente;
+        this.nroAsientos = nroAsientos;
+        this.empresa = empresa;
+        this.viajes = new ArrayList<>();
+        if (this.empresa != null) {
+            this.empresa.addBus(this);
+        }
     }
+
     public String getPatente() {
         return patente;
     }
+
     public String getMarca() {
         return marca;
     }
+
     public void setMarca(String marca) {
         this.marca = marca;
     }
+
     public String getModelo() {
         return modelo;
     }
+
     public void setModelo(String modelo) {
         this.modelo = modelo;
     }
+
     public int getNroAsientos() {
         return nroAsientos;
     }
-    public Empresa getEmpresa(){
 
-        return  empresa;
+    public Empresa getEmpresa() {
+        return empresa;
     }
 
-    public Viaje[] getViajes(){
-
-        return viajes.toArray(new Viaje[0]);
-    }
     public void addViaje(Viaje viaje) {
         if (viaje != null) {
             viajes.add(viaje);
         }
+    }
+
+    public Viaje[] getViajes() {
+        return viajes.toArray(new Viaje[0]);
     }
 }
